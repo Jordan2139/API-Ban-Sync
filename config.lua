424eef6d8f5318fb64a3e3f4640dcf4414cd10b0
Jordan.#2139 <jordansilva213@gmail.com>
2022-12-24 20:45:20 -0500

    Update to work with Firewall.gg
    This has been updated to work with the new and improved Firewall.gg

--- config.lua
+++ config.lua
@@ -3,8 +3,8 @@
 ----------------------------------------------------------------
 
 config = {
-    APIName = "https://protectthedevs.com", -- The name of your api
-    apiURL = 'https://protectthedevs.com/api/users/', -- The URL of the API that you would like to use
+    APIName = "https://firewall.hyperz.net", -- The name of your api
+    apiURL = 'https://firewall.hyperz.net/api/checkusersimple/', -- The URL of the API that you would like to use
     ThereWasAnIssue = 'Sorry, there was an issue checking against the API... Please restart FiveM and if the issue persists contact the server owner.', -- The message the user recieves if there was an issue with the API
     Discord = {
         Enabled = true, -- Would you like to enable Discord loggging?
@@ -17,6 +17,6 @@ config = {
         Wait = 10,  -- How many seconds should splash page be shown for? (Max is 12)
         Header_IMG = 'https://forum.cfx.re/uploads/default/original/3X/a/6/a6ad03c9fb60fa7888424e7c9389402846107c7e.png',
         Heading1 = "Welcome to [ServerName]",
-        Heading2 = "Make sure to join our Discord and check out our website!",    
+        Heading2 = "Make sure to join our Discord and check out our website!",
     }
 }
